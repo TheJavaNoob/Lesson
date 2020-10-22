46d81c462615cfc706914e1636d66f2576e9f43f
TheJavaNoob <3393746726@qq.com>
2020-10-22 11:07:42 +0800

    Add files via upload
    10/22/20 Commit 1

---
+++ window.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+root = tk.Tk()
+root.Btn1 = tk.Button(root)
+root.title("GCD")
+root.geometry('300x200')
+root.Btn1["text"] = "Calculate GCD"
+root.Btn1.pack(side = "bottom", pady = 20)
