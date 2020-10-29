0e8c6089c9d87331f870598bf5ae3c24b00bbe64
TheJavaNoob <3393746726@qq.com>
2020-10-29 17:36:21 +0800

    Add files via upload

--- window.py
+++ window.py
@@ -1,7 +1,15 @@
 import tkinter as tk
+
+def calculate():
+    print("Show GCD!!!")
+
 root = tk.Tk()
-root.Btn1 = tk.Button(root)
 root.title("GCD")
 root.geometry('300x200')
+
+root.Btn1 = tk.Button(root)
 root.Btn1["text"] = "Calculate GCD"
 root.Btn1.pack(side = "bottom", pady = 20)
+root.Btn1["command"] = calculate
+
+root.Txt1 = tk.
